6ec22cbf2d51c17317279485ad1c925237447bc1
adescombes <albane.descombes@epfl.ch>
2019-01-30 10:54:22 +0100

    html

---
+++ qgis2web_2019_01_30-10_32_47_522931/layers/layers.js
@@ -0,0 +1,39 @@
+var wms_layers = [];
+var baseLayer = new ol.layer.Group({
+    'title': '',
+    layers: [
+new ol.layer.Tile({
+    'title': 'Stamen Terrain',
+    'type': 'base',
+    source: new ol.source.Stamen({
+        layer: 'terrain'
+    })
+})
+]
+});
+var format_num_1850_0 = new ol.format.GeoJSON();
+var features_num_1850_0 = format_num_1850_0.readFeatures(json_num_1850_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_num_1850_0 = new ol.source.Vector({
+    attributions: [new ol.Attribution({html: '<a href=""></a>'})],
+});
+jsonSource_num_1850_0.addFeatures(features_num_1850_0);var lyr_num_1850_0 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_num_1850_0, 
+                style: style_num_1850_0,
+    title: 'num_1850<br />\
+    <img src="styles/legend/num_1850_0_0.png" /> 5 - 10<br />\
+    <img src="styles/legend/num_1850_0_1.png" /> 10 - 20<br />\
+    <img src="styles/legend/num_1850_0_2.png" /> 20 - 30<br />\
+    <img src="styles/legend/num_1850_0_3.png" /> 30 - 40<br />\
+    <img src="styles/legend/num_1850_0_4.png" /> 40 - 50<br />'
+        });
+
+lyr_num_1850_0.setVisible(true);
+var layersList = [baseLayer,lyr_num_1850_0];
+lyr_num_1850_0.set('fieldAliases', {'Nom': 'Nom', 'Métier': 'Métier', 'Rue': 'Rue', 'Numéro': 'Numéro', 'Année': 'Année', 'decade': 'decade', 'X': 'X', 'Y': 'Y', 'start': 'start', 'end': 'end', 'exercice': 'exercice', 'duration': 'duration', });
+lyr_num_1850_0.set('fieldImages', {'Nom': 'TextEdit', 'Métier': 'TextEdit', 'Rue': 'TextEdit', 'Numéro': 'TextEdit', 'Année': 'TextEdit', 'decade': 'TextEdit', 'X': 'TextEdit', 'Y': 'TextEdit', 'start': 'TextEdit', 'end': 'TextEdit', 'exercice': 'TextEdit', 'duration': 'TextEdit', });
+lyr_num_1850_0.set('fieldLabels', {'Nom': 'inline label', 'Métier': 'header label', 'Rue': 'no label', 'Numéro': 'no label', 'Année': 'no label', 'decade': 'no label', 'X': 'no label', 'Y': 'no label', 'start': 'no label', 'end': 'no label', 'exercice': 'inline label', 'duration': 'no label', });
+lyr_num_1850_0.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ qgis2web_2019_01_30-10_32_47_522931/styles/num_1850_0_style.js
@@ -0,0 +1,59 @@
+var size = 0;
+var placement = 'point';
+
+var style_num_1850_0 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("duration");
+    var labelText = "";
+    size = 0;
+    var labelFont = "16.9px \'.SF NS Text\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value > 5.000000 && value <= 10.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(0,0,0,0.83)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 10.000000 && value <= 20.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(124,0,0,0.83)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 20.000000 && value <= 30.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(213,63,43,0.83)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 30.000000 && value <= 40.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(248,128,81,0.83)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 40.000000 && value <= 50.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(252,183,89,0.83)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    };
+
+    return style;
+};
